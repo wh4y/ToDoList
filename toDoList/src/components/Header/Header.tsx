470f3fde0a32cecfc884a302d4928edf741563c3
wh4y <wh4y@mail.ru>
2021-11-09 00:40:06 +0300

    UseRef input value picker added

--- toDoList/src/components/Header/Header.tsx
+++ toDoList/src/components/Header/Header.tsx
@@ -1,18 +1,26 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 import { Input } from "antd";
 import styles from './styles/style.module.scss';
 import { PlusOutlined } from "@ant-design/icons";
 
 const Header: FC = () => {
+
+    const inputElement = useRef<Input>(null);
+
+    const handleClick = () => {
+        console.log(inputElement.current!.state.value);
+    }
+
     return (
         <div className={styles.main__header}>
             <Input
                 className={styles.toDo__input}
+                ref={inputElement}
                 placeholder={'New toDo :)'}
             />
             <PlusOutlined
                 className={styles.add__btn}
-                onClick={() => console.log('jopa')}
+                onClick={handleClick}
                 twoToneColor={'#1890ff'}
             />
         </div>
