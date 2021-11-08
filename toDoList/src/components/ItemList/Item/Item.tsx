0bdf6b33f2f8cb3e5a202ae4b91709a355b047ba
wh4y <wh4y@mail.ru>
2021-11-08 23:55:03 +0300

    List Item added, common button hover-active mixin added

--- toDoList/src/components/ItemList/Item/Item.tsx
+++ toDoList/src/components/ItemList/Item/Item.tsx
@@ -1,7 +1,14 @@
+import { DeleteOutlined } from "@ant-design/icons";
+import { FC } from "react";
+import styles from "./styles/styles.module.scss";
 
-const Item = () => {
-    return(
-        <span>ITEM</span>
+
+const Item: FC = () => {
+    return (
+        <div className={styles.item__wrapper}>
+            <span className={styles.text}>TEXT</span>
+            <DeleteOutlined className={styles.delete__btn} />
+        </div>
     );
 }
 
