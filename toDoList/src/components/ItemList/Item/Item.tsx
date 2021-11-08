e7c80547691e4a7326ad22239a520815025345bd
wh4y <wh4y@mail.ru>
2021-11-09 00:18:28 +0300

    Item modal created

--- toDoList/src/components/ItemList/Item/Item.tsx
+++ toDoList/src/components/ItemList/Item/Item.tsx
@@ -1,14 +1,38 @@
 import { DeleteOutlined } from "@ant-design/icons";
-import { FC } from "react";
+import { Modal } from "antd";
+import { FC, useState } from "react";
 import styles from "./styles/styles.module.scss";
 
 
 const Item: FC = () => {
+
+    const [isModalOpen, SetIsModalOpen] = useState(false);
+
+    const toggleModalMode = () => {
+        SetIsModalOpen(!isModalOpen);
+    }
+
     return (
-        <div className={styles.item__wrapper}>
-            <span className={styles.text}>TEXT</span>
-            <DeleteOutlined className={styles.delete__btn} />
-        </div>
+        <>
+            <Modal
+                visible={isModalOpen}
+                onCancel={toggleModalMode}
+                footer={null}
+            >
+                JOPA
+            </Modal>
+
+            <div
+                className={styles.item__wrapper}
+                onClick={toggleModalMode}
+            >
+                <span className={styles.text}>TEXT</span>
+                <DeleteOutlined
+                    className={styles.delete__btn}
+                    onClick={e => e.stopPropagation()}
+                />
+            </div>
+        </>
     );
 }
 
