35b17ce85568d1c70a3c9d3d48434e496b009235
wh4y <wh4y@mail.ru>
2021-11-09 15:12:47 +0300

    no toDo message added, made it uselectable(header logo as well)

--- toDoList/src/components/ItemList/ItemList.tsx
+++ toDoList/src/components/ItemList/ItemList.tsx
@@ -1,9 +1,11 @@
+import { Typography } from "antd";
 import { FC } from "react";
 import { shallowEqual, useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 import Item from "./Item/Item";
 import styles from "./styles/styles.module.scss";
 
+const { Text } = Typography;
 
 const ItemList: FC = () => {
 
@@ -11,7 +13,10 @@ const ItemList: FC = () => {
 
     return (
         <div className={styles.list__wrapper}>
-            {itemList.map((elem) => <Item {...elem} key={elem.id} />)}
+            {
+                !itemList[0] ? <Typography><Text className={styles.no__toDo}>Add your first to Do :)</Text></Typography> :
+                itemList.map((elem) => <Item {...elem} key={elem.id} />)
+            }
         </div>
     );
 }
