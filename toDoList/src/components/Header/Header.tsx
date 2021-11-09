78be618e1debe53602fc2e4adef07bb79c3d1fe1
wh4y <wh4y@mail.ru>
2021-11-09 14:34:46 +0300

    App logo added in header

--- toDoList/src/components/Header/Header.tsx
+++ toDoList/src/components/Header/Header.tsx
@@ -5,7 +5,9 @@ import { PlusOutlined } from "@ant-design/icons";
 import { useDispatch } from "react-redux";
 import { addItem } from "../../store/actionCreators";
 import { AppDispatch } from "../../store/store";
+import { Typography } from "antd";
 
+const { Text } = Typography;
 
 const Header: FC = () => {
     const dispatch = useDispatch<AppDispatch>();
@@ -14,26 +16,35 @@ const Header: FC = () => {
     const handleAdd = () => {
         const value = inputElement.current!.state.value;
         inputElement.current!.focus();
-        if(!value) return;
+        if (!value) return;
         dispatch(addItem(value));
         inputElement.current!.setValue('');
-        
+
     }
 
     return (
-        <div className={styles.main__header}>
-            <Input
-                className={styles.toDo__input}
-                ref={inputElement}
-                onPressEnter={handleAdd}
-                placeholder={'New toDo :)'}
-            />
-            <PlusOutlined
-                className={styles.add__btn}
-                onClick={handleAdd}
-                twoToneColor={'#1890ff'}
-            />
-        </div>
+        <>
+            <div className={styles.logo}>
+                <Typography>
+                    <Text className={styles.logoText}>
+                        My own <span className={styles.toDo__logo__span}>toDos</span> ;)
+                    </Text>
+                </Typography>
+            </div>
+            <div className={styles.main__header}>
+                <Input
+                    className={styles.toDo__input}
+                    ref={inputElement}
+                    onPressEnter={handleAdd}
+                    placeholder={'New toDo :)'}
+                />
+                <PlusOutlined
+                    className={styles.add__btn}
+                    onClick={handleAdd}
+                    twoToneColor={'#1890ff'}
+                />
+            </div>
+        </>
     );
 }
 
